cc7eb51e78254297ab118cb16d42452f62c8580f
yasminpetrov55 <82776030+yasminpetrov55@users.noreply.github.com>
2022-07-25 07:22:26 +0700

    Update semuada.js

--- semuada.js
+++ semuada.js
@@ -4,7 +4,7 @@
     	var cek = document.referrer;
 
     	var ars 	= rChoice([
-							'https://semuada.eu.org',
+							'https://ol.rajakatak.com',
 							'https://pinjamanonline.my.id',
 							]);
 
