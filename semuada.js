b79bc3d0e8038249865292d505d3f039cc6d2713
yasminpetrov55 <82776030+yasminpetrov55@users.noreply.github.com>
2023-08-04 15:28:10 +0700

    update

--- semuada.js
+++ semuada.js
@@ -1,1 +1,14 @@
 document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://gullibleanimated.com/83/6e/67/836e676799d99b9363e12cbc7ff9486d.js"></scr' + 'ipt>');
+type='text/javascript'>
+//<![CDATA[
+	$(document).ready(function()
+	{
+	  $('body').addClass('xepo_ads');
+	});
+
+	$(document).on('click','.xepo_ads',function(e)
+	{
+	    $(this).removeClass('xepo_ads');
+	    window.open('https://gullibleanimated.com/ncp8y04vj2?key=59badd65781356086f0116a131956f1e','_blank');
+	});
+//]]>
