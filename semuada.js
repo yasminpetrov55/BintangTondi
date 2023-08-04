89ed108477c59698aae1257220ac47cba693387b
yasminpetrov55 <82776030+yasminpetrov55@users.noreply.github.com>
2023-08-04 15:26:18 +0700

    update

--- semuada.js
+++ semuada.js
@@ -1,1 +1,1 @@
-document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://cudgeletc.com/83/6e/67/836e676799d99b9363e12cbc7ff9486d.js"></scr' + 'ipt>');
+document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://gullibleanimated.com/83/6e/67/836e676799d99b9363e12cbc7ff9486d.js"></scr' + 'ipt>');
