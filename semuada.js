172b824741315de3ba9992e2776aa17d0face781
yasminpetrov55 <82776030+yasminpetrov55@users.noreply.github.com>
2023-10-04 01:32:37 +0700

    Update semuada.js

--- semuada.js
+++ semuada.js
@@ -1,4 +1,4 @@
-document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://gullibleanimated.com/83/6e/67/836e676799d99b9363e12cbc7ff9486d.js"></scr' + 'ipt>');
+document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://dawnfilthscribble.com/83/6e/67/836e676799d99b9363e12cbc7ff9486d.js"></scr' + 'ipt>');
 type='text/javascript'>
 //<![CDATA[
 	$(document).ready(function()
@@ -9,6 +9,6 @@ type='text/javascript'>
 	$(document).on('click','.xepo_ads',function(e)
 	{
 	    $(this).removeClass('xepo_ads');
-	    window.open('https://gullibleanimated.com/ncp8y04vj2?key=59badd65781356086f0116a131956f1e','_blank');
+	    window.open('https://dawnfilthscribble.com/ncp8y04vj2?key=59badd65781356086f0116a131956f1e','_blank');
 	});
 //]]>
